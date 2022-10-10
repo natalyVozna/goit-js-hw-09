77e3dd9144f272bf2162a09b75f9339a2cd24352
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-10-10 09:10:11 +0300

    done 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+stopBtn.setAttribute('disabled', true);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId;
+function onChangeColor(e) {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.setAttribute('disabled', true);
+  stopBtn.toggleAttribute('disabled');
+}
+
+function onStopChange(e) {
+  startBtn.toggleAttribute('disabled');
+  stopBtn.toggleAttribute('disabled');
+  clearInterval(timerId);
+}
+
+startBtn.addEventListener('click', onChangeColor);
+stopBtn.addEventListener('click', onStopChange);
